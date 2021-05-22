947f6228c00b2f68b8c43a8ec381e6cd4635eaf5
Gilberto Martins <gilbertomartins643@gmail.com>
2021-05-22 12:51:15 -0300

    init

---
+++ Documents/MeuProjeto/AppSeries/AppsSeries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppsSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
