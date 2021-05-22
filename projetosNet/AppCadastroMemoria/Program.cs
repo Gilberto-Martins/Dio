24da3daaf57c5861b92d240a216a66859c239414
Gilberto Martins <gilbertomartins643@gmail.com>
2021-05-22 12:27:16 -0300

    init

---
+++ projetosNet/AppCadastroMemoria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppCadastroMemoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello neto");
+        }
+    }
+}
